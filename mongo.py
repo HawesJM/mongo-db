6defdbd5ffe57bad32d952f3135169718ab3bf68
HawesJM <>
2023-12-20 16:34:42 +0000

    added main menu

--- mongo.py
+++ mongo.py
@@ -22,7 +22,51 @@ conn = mongo_connect(MONGO_URI)
 
 coll = conn[DATABASE][COLLECTION]
 
+#inserts with every iteration, run
+
+# new_doc = {"first": "douglas", "last": "adams", "dob": "11/03/1952", "hair_color": "grey", "occupation": "writer", "nationality": "british"}
+
+#coll.insert_one(new_doc)
+
+#
+#new_docs = [{
+#    "first": "terry",
+#    "last": "pratchett",
+#    "dob": "28/04/1948",
+#    "gender": "m",
+#    "hair_color": "not much",
+#    "occupation": "writer",
+#    "nationality": "british"
+#}, {
+#    "first": "george",
+#    "last": "rr martin",
+#    "dob": "20/09/1948",
+#    "gender": "m",
+#    "hair_color": "white",
+#    "occupation": "writer",
+#    "nationality": "american"
+##}]
+
+#coll.insert_many(new_docs)
+
 documents = coll.find()
+
+# finding something specific
+
+#documents = coll.find({"first": "douglas"})
+
+# removing something specific
+
+coll.delete_many({"first": "douglas"})
+
+#douglas adams removed
+#documents = coll.find()
+
+#update information (singular)
+
+coll.update_one({"nationality": "american"}, {"$set": {"hair_color": "maroon"}})
+
+documents = coll.find({"nationality": "american"})
 
 for doc in documents: 
     print(doc)
